79dbfe4c5e6792bcddfddd32a3cb0d6c7c39cd5d
nhanis <n.khanis@it.ecommpay.com>
2021-10-06 01:00:39 +0300

    add auth

---
+++ nuxt.config.js
@@ -0,0 +1,107 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Count',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseURL: 'http://91.226.116.39',
+    credentials: true
+  },
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: '/api/auth/login', method: 'post', propertyName: 'data' },
+          user: { url: '/api/auth/user', method: 'get', propertyName: false }
+        },
+        tokenRequired: false,
+        tokenType: false
+      },
+      laravelSanctum: {
+        provider: 'laravel/sanctum',
+        url: 'http://91.226.116.39',
+        endpoints: {
+          login: { url: '/api/auth/login', method: 'post' }
+        },
+        tokenRequired: false,
+        tokenType: false
+      }
+    },
+    localStorage: false,
+    redirect: {
+      login: '/login',
+      logout: '/',
+      callback: '/login',
+      home: '/'
+    }
+  },
+  // strategies: {
+  //   local: {
+  //     endpoints: {
+  //       login: { url: 'http://91.226.116.39/api/auth/login', method: 'post', propertyName: 'token' }
+  //     }
+  //   },
+  //   laravelSanctum: {
+  //     provider: 'laravel/sanctum',
+  //     url: 'http://91.226.116.39',
+  //     endpoints: {
+  //       login: {
+  //         url: '/api/auth/login',
+  //         withCredentials: true,
+  //         method: 'post'
+  //       }
+  //     }
+  //   }
+  // },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
