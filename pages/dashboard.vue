73105b2c7151a306c23762e632e83218371c5dce
Ханис Николай <hn@wnstudio.ru>
2021-11-28 18:07:01 +0300

    add css for dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,26 +1,50 @@
 <template>
-  <div class="main dashboard">
-    <p>DESCRIPTION: {{ $route.name }}</p>
-    <ul class="list-group">
-      <li
-        v-for="shield in elist"
-        :key="shield.id"
-        class="list-group-item"
-      >
-        <nav-item
-          :link="{
-            name: 'Шкаф ' + shield.name,
-            multiply: true,
-            icon: 'el-icon-s-grid',
-            params: {
-              url: 'shield-path',
-              id: shield.id,
-              path: shield.name,
-            }
-          }"
-        />
-      </li>
-    </ul>
+  <div class="main">
+    <div class="container-fluid dashboard">
+      <h1>Мониторинг</h1>
+      <div class="d-flex align-items-center">
+        <div class="status-block online d-flex">
+          <div class="status-img">
+            <div>
+              <img src="/thunder.png">
+            </div>
+          </div>
+          <div class="status-detail">
+            <span>50</span> в сети
+          </div>
+        </div>
+        <div class="status-block offline d-flex">
+          <div class="status-img">
+            <div>
+              <img src="/offline.png">
+            </div>
+          </div>
+          <div class="status-detail">
+            <span>0</span> не в сети
+          </div>
+        </div>
+      </div>
+      <ul class="list-group">
+        <li
+          v-for="shield in elist"
+          :key="shield.id"
+          class="list-group-item"
+        >
+          <nav-item
+            :link="{
+              name: 'Шкаф ' + shield.name,
+              multiply: true,
+              icon: 'el-icon-s-grid',
+              params: {
+                url: 'shield-path',
+                id: shield.id,
+                path: shield.name,
+              }
+            }"
+          />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -43,8 +67,8 @@ export default {
   // Другие вычисляемые свойства
   },
   mounted () {
-    console.log(this.$store.state.elist)
     this.$store.dispatch('elist/fetchSheilds')
+    console.log(this.$store.state.elist)
   }
 }
 </script>
